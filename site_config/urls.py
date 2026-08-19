b243e1e7e295ab9f3f0ade6126d0f7096495b1b0
fahiiim <fahimsarker0805@gmail.com>
2026-08-19 12:51:01 +0600

    Kosa Project

---
+++ site_config/urls.py
@@ -0,0 +1,56 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve as static_serve
+import os
+from django.http import JsonResponse
+from django.utils.timezone import now
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+# Administrator Dashboard Customized---
+admin.site.site_title = "Tatto Hysteria"
+admin.site.site_header = "Tatto Hysteria"
+admin.site.app_index = "Welcome to Tatto Hysteria Administrator"
+
+# Main Base API Online Template
+def Home(request):
+    return JsonResponse({
+        "application": "Tatto Hysteria API",
+        "status": "online 🚀",
+        "server_time": now(),
+        "message": "Backend server is running successfully."
+    })
+
+def api_endpoint(request):
+    return JsonResponse(
+        {
+            "application": "API V1 Endpoint!",
+            "status": "online 🚀",
+            "server_time": now(),
+        }
+    )
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", Home, name="WelcomeAPI"),
+
+    # app urls include
+    path("api/v1/", api_endpoint, name="api_endpoint"),
+    path("api/v1/", include("account.urls")),
+    path("api/v1/", include("core.urls")),
+
+    # API schema
+    path("api/schema/", SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Redoc UI
+    path("api/redoc/", SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
+
+SERVE_MEDIA = os.getenv("SERVE_MEDIA", "False").strip().lower() in ("true","1","yes")
+
+if SERVE_MEDIA:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', static_serve, {'document_root': settings.STATIC_ROOT}),
+    ]
